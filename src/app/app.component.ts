e06a91e3ed66079292bae737dcfe3c6dce970bdc
LAPTOP-ISJBUOG1\sfvge <sfvgekko@gmail.com>
2023-12-12 03:29:12 +0100

    fin seccoin 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CounterComponent } from './counter/components/counter/counter.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-bases';
+  public title: string = 'Mi primera app de angular';
 }
---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: ` <h3>Counter: {{ counter }}</h3>
+    <button (click)="increaseBy(5)">+1</button>
+    <button (click)="resetCounter()">Reset</button>
+    <button (click)="increaseBy(-1)">-1</button>`,
+})
+export class CounterComponent {
+  public counter: number = 2;
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+
+  resetCounter(): void {
+    this.counter = 10;
+  }
+}
